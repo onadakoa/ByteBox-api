624115fd2cacb77214fae4bbef52e992c54278e1
onadakoa <onadakoa@gmail.com>
2025-04-27 22:12:50 +0200

    Attachment fetch_all

--- php/vendor/Attachment.php
+++ php/vendor/Attachment.php
@@ -4,7 +4,7 @@ class Attachment
     public int $attachment_id;
     public int $image_count;
     public int $author_id;
-    public int $creation_date;
+    public string $creation_date;
     public array $images = [];
 
     private mysqli $db;
@@ -30,6 +30,27 @@ class Attachment
             $out->images[] = new Image($row['image_id'], $id, $row['path'], $row['size']);
         } while($row = $result->fetch_assoc());
 
+        return $out;
+    }
+
+    /**
+     * @return Attachment[]|false
+     */
+    public static function fetch_all(mysqli $db, int $limit = 10, int $offset = 0): array|false {
+        $query = "select *, unix_timestamp(creation_date) as creation_date from attachment limit $limit offset $offset";
+        $res = $db->query($query);
+        if ($res->num_rows < 1) return false;
+        $out = [];
+
+        while ($row = $res->fetch_assoc()) {
+            $tmp = new Attachment($db, $row['attachment_id'], $row['image_count'], $row['author_id'], $row['creation_date']);
+
+            $img_res = $db->query("select * from image where attachment_id={$row['attachment_id']}");
+            while ($img_row = $img_res->fetch_assoc()) {
+                $tmp->images[] = new Image($img_row['image_id'], $img_row['attachment_id'], $img_row['path'], $img_row['size']);
+            }
+            $out[] = $tmp;
+        }
         return $out;
     }
 
