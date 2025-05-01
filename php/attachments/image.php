d6c3991a82036831f1e75f62e877dd2532256b07
onadakoa <onadakoa@gmail.com>
2025-05-01 22:15:38 +0200

    image.php in PUT method replaced badRequest -> badRequestJson

--- php/attachments/image.php
+++ php/attachments/image.php
@@ -32,10 +32,10 @@ function PUT() {
     session_start();
 
     $id = (int) $_GET['id'] ?? -1;
-    if ($id < 0) badRequest("no id specified", 400);
+    if ($id < 0) badRequestJson("no id specified", 400);
     $content = file_get_contents("php://input");
     $size = $_SERVER['CONTENT_LENGTH'];
-    if ($content === false || $size == 0) badRequest("wrong body", 400);
+    if ($content === false || $size == 0) badRequestJson("wrong body", 400);
 
     $db = get_mysqli();
 
@@ -44,11 +44,11 @@ function PUT() {
     finfo_close($finfo);
 
     $image = Image::fetch_image($db, $id);
-    if (!$image) badRequest("not found");
+    if (!$image) badRequestJson("not found");
 
     $newPath = "/" . date("d_m_Y") . "_" . bin2hex(random_bytes(8)) . ".image";
 
-    if (!file_put_contents($PATH . $newPath, $content)) badRequest("file write error", 500);
+    if (!file_put_contents($PATH . $newPath, $content)) badRequestJson("file write error", 500);
     $image->update($newPath, $type, $size);
 
     echo new Packet(ResponseCode::SUCCESS, "success update");
