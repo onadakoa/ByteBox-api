842670aff84f77671436d6787aadf1ff70c93b2c
onadakoa <onadakoa@gmail.com>
2025-05-01 16:05:10 +0200

    Image: `fill`, `exists`, `update` methods

--- php/vendor/Image.php
+++ php/vendor/Image.php
@@ -4,11 +4,11 @@ class Image
 {
     public int $id;
     private int $attachment_id;
-    private string $path;
-    public int $size;
-    public string $type;
+    private string|null $path;
+    public int|null $size;
+    public string|null $type;
 
-    public function __construct(int $id, int $attachment_id, string $path, int $size, string $type) {
+    public function __construct(int $id, int $attachment_id, $path, $size, $type) {
         $this->id = $id;
         $this->attachment_id = $attachment_id;
         $this->path = $path;
@@ -24,6 +24,52 @@ class Image
     public function getPath(): string
     {
         return $this->path;
+    }
+    public function fill(int $newID=null): bool {
+        $id = $newID ?? $this->id;
+        $db = get_mysqli();
+
+        $res = $db->query("select * from image where image_id=$id");
+        if ($res->num_rows != 1) return false;
+        $row = $res->fetch_assoc();
+
+        $this->id = $row['image_id'];
+        $this->attachment_id = $row['attachment_id'];
+        $this->path = $row['path'];
+        $this->size = $row['size'];
+        $this->type = $row['type'];
+
+        $db->close();
+        return true;
+    }
+
+    public function exists(): bool {
+        $db = get_mysqli();
+        $res = $db->query("select * from image where image_id=" . $this->id);
+        $db->close();
+        if ($res->num_rows != 1) return false;
+        return true;
+    }
+
+    public function update(string $path, string $type, int $size = -1): bool {
+        if (!$this->exists()) return false;
+        $db = get_mysqli();
+
+        $sizeSTM = ($size>=0) ? ", size=$size" : "";
+        $query = <<<sql
+        update image set
+                         path='$path',
+                         type='$type'
+                         $sizeSTM
+        where image_id={$this->id}
+        sql;
+
+        $res = $db->query($query);
+        if (!$res) return false;
+
+        $db->close();
+        $this->fill();
+        return true;
     }
 
     public static function fetch_image(mysqli $db, $id) {
