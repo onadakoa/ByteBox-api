78cb7d47b497b84b90067ceac42f9d5014b71756
onadakoa <onadakoa@gmail.com>
2025-05-02 22:45:47 +0200

    Products POST method

--- php/products/index.php
+++ php/products/index.php
@@ -25,4 +25,41 @@ function GET() {
     $db->close();
 }
 
+function POST() { // {name, description, attachment_id?, author_id, price, stock, category_id}
+    session_start();
+    useJson();
+    $required = ["name", "description", "author_id", "price", "stock", "category_id"];
+    $obj = [];
+    foreach ($required as $field) {
+        if (!isset($_POST[$field])) badRequestJson("missing field $field", 400);
+        $obj[$field] = $_POST[$field];
+    }
+    $obj['attachment_id'] = $_POST['attachment_id'] ?? null;
+
+    $db = get_mysqli();
+
+    $query = <<<sql
+    insert into product (name, description, attachment_id , author_id, price, stock, category_id)
+    value (?, ?, ?, ?, ?, ?, ?)
+    sql;
+
+    $stmt = $db->prepare($query);
+
+    $stmt->bind_param("ssiidii",
+    $obj['name'], $obj['description'], $obj['attachment_id'], $obj['author_id'], $obj['price'], $obj['stock'], $obj['category_id']
+    );
+
+    try {
+        if (!$stmt->execute()) badRequestJson("server error", 500);
+    } catch (mysqli_sql_exception $e) {
+        badRequestJson("server error, {$e->getMessage()}", 500);
+    }
+
+    $nId = $db->insert_id;
+
+    echo new Packet(ResponseCode::SUCCESS, ["id" => $nId]);
+
+    $db->close();
+}
+
 handleRequest();
