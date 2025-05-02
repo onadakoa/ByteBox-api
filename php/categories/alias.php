815eddc6b720e2b7f80e909d76e41fe215110f0e
onadakoa <onadakoa@gmail.com>
2025-05-03 00:16:41 +0200

    /categories/alias.php route PUT

--- php/categories/alias.php
+++ php/categories/alias.php
@@ -34,4 +34,29 @@ function POST() { // {alias, category_id?}
     $db->close();
 }
 
+function PUT() { // {alias, category_id}
+    useJson();
+    $body = [];
+    parse_str(file_get_contents("php://input"), $body);
+    $id = $body['id'] ?? -1;
+    if ($id < 0) badRequestJson("bad id", 400);
+
+    if (!isset($body['alias'])) badRequestJson("no alias", 400);
+    $alias = $body['alias'];
+    if (!isset($body['category_id'])) badRequestJson("no category_id", 400);
+    $category_id = $body['category_id'];
+
+    $db = get_mysqli();
+    try {
+        $stm = $db->prepare("update category_alias set alias=?, category_id=? where alias_id=?");
+        $stm->bind_param("sii", $alias, $category_id, $id);
+        if (!$stm->execute()) badRequestJson("error", 500);
+    } catch (mysqli_sql_exception $e) {
+        badRequestJson("error, {$e->getMessage()}", 500);
+    }
+
+    echo new Packet(ResponseCode::SUCCESS);
+    $db->close();
+}
+
 handleRequest();
