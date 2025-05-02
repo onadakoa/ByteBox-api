8663ab6655cac9364485265c3ef5d7f751252be2
onadakoa <onadakoa@gmail.com>
2025-05-02 23:40:15 +0200

    /categories route GET method

--- php/categories/index.php
+++ php/categories/index.php
@@ -1,27 +1,25 @@
 <?php
 require_once "../autoload.php";
-use_request_method(RequestMethod::GET->value);
 
-$db = get_mysqli();
+function GET()
+{
+    useJson();
+    if (isset($_GET["id"]) && !ctype_digit($_GET['id'])) badRequestJson("bad id");
+    $id = $_GET["id"] ?? -1;
 
-$out = [];
-$res = $db->query("select c.category_id, c.name, a.alias_id, a.alias from category c left join category_alias a on c.category_id=a.category_id order by c.category_id");
+    $db = get_mysqli();
 
-while ($row = $res->fetch_assoc()) {
-    if (!isset($out[$row['category_id']])) {
-       $out[$row['category_id']] = array(
-          'category_id' => $row['category_id'],
-          'name' => $row['name'],
-          'alias' => []
-       );
+    if ($id == -1) {
+        $cats = Category::fetch_all($db);
+        if (!$cats) badRequestJson("error", 500);
+        echo new Packet(ResponseCode::SUCCESS, $cats);
+        $db->close();
+        return;
     }
-
-    if ($row['alias']) {
-        $out[$row['category_id']]['alias'][] = array(
-            'alias_id' => $row['alias_id'],
-            'alias' => $row['alias'],
-        );
-    }
+    $cat = Category::fetch_category($db, $id);
+    if (!$cat) badRequestJson("not found", 404);
+    echo new Packet(ResponseCode::SUCCESS, $cat);
+    $db->close();
 }
 
-echo new Packet(ResponseCode::SUCCESS, array_values($out));
+handleRequest();
