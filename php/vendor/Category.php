126c0a384123363300cf4ab4f9f46bbba2390599
onadakoa <onadakoa@gmail.com>
2025-05-02 23:37:05 +0200

    Category fetch_all method

--- php/vendor/Category.php
+++ php/vendor/Category.php
@@ -36,4 +36,24 @@ class Category
         }
         return $out;
     }
+
+    /**
+     * @return Category[]|false
+     */
+    public static function fetch_all(mysqli $db) {
+        $res = $db->query("select * from category");
+        if (!$res) return false;
+
+        $out = [];
+        while ($row = $res->fetch_assoc()) {
+            $obj = new Category($row["category_id"], $row["name"]);
+            $AliasRes = $db->query("select * from category_alias where category_id={$row['category_id']}");
+            if (!$AliasRes) continue;
+            while ($alias = $AliasRes->fetch_assoc()) {
+                $obj->alias[] = array("name" => $alias["alias"], "alias_id" => $alias["alias_id"]);
+            }
+            $out[] = $obj;
+        }
+        return $out;
+    }
 }
