dfe6c0804be89a063573f82d0d78dfbc79e4c353
onadakoa <onadakoa@gmail.com>
2025-05-02 21:28:35 +0200

    attachments route POST method (creating new attachments)

--- php/attachments/index.php
+++ php/attachments/index.php
@@ -1,7 +1,6 @@
 <?php
 require_once "../autoload.php";
 session_start();
-
 function GET() {
     $db = get_mysqli();
     $id = (int) ($_GET['id'] ?? -1);
@@ -27,6 +26,52 @@ function GET() {
 
     echo new Packet(ResponseCode::SUCCESS, $att);
     exit();
+}
+
+function POST() {
+    header("Content-Type: application/json");
+
+    $db = get_mysqli();
+    $token = $_SESSION['TOKEN'] ?? -1;
+    $user = User::user_by_token($db, $token);
+    if (!$user || $user->permission == 0) badRequestJson("no auth", 400);
+
+    $file_count = $_POST['file_count'] ?? 0;
+    if ($file_count < 1 || $file_count > MAX_ATTACHMENT_FILES) badRequestJson("wrong file_count", 400);
+
+    $files = json_decode($_POST['files'] ?? null, true); // [{file_id, size, type}]
+    if (!$files || !is_array($files)) badRequestJson("wrong files", 400);
+
+    foreach ($files as $f) {
+        $id = $f['file_id'] ?? -1;
+        $size = $f['size'] ?? 0;
+        $type = $f['type'] ?? null;
+        if (!in_array($type, ALLOW_MIME)) badRequestJson("file id=$id, wrong mimetype", 400);
+        if ($size > MAX_SIZE) badRequestJson("file id=$id, size > MAX_SIZE", 400);
+    }
+
+    $query = "insert into attachment (author_id, image_count) values (4, $file_count)";
+    $res = $db->query($query);
+    if (!$res) badRequestJson("error", 500);
+    $rowID = $db->insert_id;
+
+    $out = [];
+
+    foreach ($files as $f) {
+        $query = "insert into image (attachment_id) values ($rowID)";
+        $res = $db->query($query);
+        if (!$res) badRequestJson("error", 500);
+        $out[] = [
+            "file_id" => ($f['id'] ?? -1),
+            "id" => $db->insert_id
+        ];
+    }
+
+    $db->close();
+    echo new Packet(ResponseCode::SUCCESS, [
+        "files" => $out,
+        "attachment_id" => $rowID,
+    ]);
 }
 
 handleRequest();
