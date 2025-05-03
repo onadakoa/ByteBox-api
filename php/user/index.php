4ea6dd902a987d9654f85113d1c0c14c85126ca4
onadakoa <onadakoa@gmail.com>
2025-05-03 23:09:00 +0200

    /user route PUT, DELETE

--- php/user/index.php
+++ php/user/index.php
@@ -5,7 +5,6 @@ function GET() {
     session_start();
     useJson();
     $token = useToken();
-    if (!$token) badRequestJson("no auth", 400);
 
     $id = $_GET['id'] ?? -1;
     $search = $_GET['search'] ?? "";
@@ -30,6 +29,52 @@ function GET() {
         echo new Packet(ResponseCode::SUCCESS, $author);
     }
 
+    $db->close();
+}
+
+function PUT() { // {id, login, password, first_name, last_name, permission}
+    session_start();
+    useJson();
+    $token = useToken();
+    $body = useFormData();
+
+    $db = get_mysqli();
+    $author = User::user_by_token($db, $token);
+    if (!$author) badRequestJson("invalid token", 400);
+
+    $id = $_GET['id'] ?? $author->user_id;
+    $target = User::user_by_id($db, $id);
+    if (!$target) badRequestJson("not found");
+
+    if (!$target->update($db, $body['login'] ?? null, $body['password'] ?? null, $body)) badRequestJson("error", 500);
+    echo new Packet(ResponseCode::SUCCESS, $target);
+    $db->close();
+}
+
+function DELETE() {
+    session_start();
+    useJson();
+    $token = useToken();
+
+    $id = $_GET['id'] ?? -1;
+    $db = get_mysqli();
+    $author = User::user_by_token($db, $token);
+    if (!$author) badRequestJson("invalid token", 400);
+
+    if ($id==-1) {
+        if (!$author->delete($db)) badRequestJson("error", 500);
+        unset($_SESSION['TOKEN']);
+        echo new Packet(ResponseCode::SUCCESS);
+        $db->close();
+        exit();
+    }
+
+    if ($author->permission == 0) badRequestJson("no permission", 400);
+    $target = User::user_by_id($db, $id);
+    if (!$target) badRequestJson("not found");
+
+    if (!$target->delete($db)) badRequestJson("error", 500);
+    echo new Packet(ResponseCode::SUCCESS);
     $db->close();
 }
 
