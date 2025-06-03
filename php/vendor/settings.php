6cc0b73bc7851e628dd367cf82b977370b81702d
onadakoa <onadakoa@gmail.com>
2025-06-03 10:16:01 +0200

    added mime type

--- php/vendor/settings.php
+++ php/vendor/settings.php
@@ -1,4 +1,4 @@
 <?php
-const ALLOW_MIME = ["image/png", "image/jpeg", "image/jpg"];
+const ALLOW_MIME = ["image/png", "image/jpeg", "image/jpg", "image/webp"];
 const MAX_SIZE = 5242880; // 5MB
 const MAX_ATTACHMENT_FILES = 4;
