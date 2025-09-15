60e6c550f57a1e2dc42561c4a7a705eb2b494e60
onadakoa <onadakoa@gmail.com>
2025-09-15 20:25:34 +0200

    settings.php const changed to define for dynamic database host (from environment variable), index.php changed to handleRequest

--- php/index.php
+++ php/index.php
@@ -1,17 +1,22 @@
 <?php
 require_once "autoload.php";
-session_start();
-use_request_method(RequestMethod::GET->value | RequestMethod::POST->value);
-$db = get_mysqli();
-$out = array();
 
-if (isset($_SESSION['TOKEN'])) {
-    $u = User::user_by_token($db, $_SESSION['TOKEN']);
-    $out['user'] = $u;
-} else
-    $out['user'] = 0;
+function GET() {
+    session_start();
+    $db = get_mysqli();
+    $out = array();
+
+    if (isset($_SESSION['TOKEN'])) {
+        $u = User::user_by_token($db, $_SESSION['TOKEN']);
+        $out['user'] = $u;
+    } else
+        $out['user'] = 0;
 
 
-echo new Packet(ResponseCode::SUCCESS, $out);
+    echo new Packet(ResponseCode::SUCCESS, $out);
 
-$db->close();
+    $db->close();
+
+}
+
+handleRequest();
--- php/vendor/settings.php
+++ php/vendor/settings.php
@@ -2,4 +2,4 @@
 const ALLOW_MIME = ["image/png", "image/jpeg", "image/jpg", "image/webp"];
 const MAX_SIZE = 5242880; // 5MB
 const MAX_ATTACHMENT_FILES = 4;
-const HOSTNAME = "localhost";
+define("HOSTNAME", getenv("MYSQL_HOST") ?? "localhost");
